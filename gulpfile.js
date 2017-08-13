8ffe238230397519142d87998678bc41d5731802
Evgeny <16515461+evgn125@users.noreply.github.com>
2017-08-13 18:09:40 +0500

    first

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+"use strict";
+
+var gulp = require("gulp"),
+  autoprefixer = require("autoprefixer"),
+  combineMq = require("gulp-combine-mq"),
+  concat = require("gulp-concat"),
+  cssmin = require("gulp-cssmin"),
+  htmlmin = require("gulp-htmlmin"),
+  imagemin = require("gulp-imagemin"),
+  plumber = require("gulp-plumber"),
+  postcss = require("gulp-postcss"),
+  sass = require("gulp-sass"),
+  uglify = require("gulp-uglify"),
+  uncss = require("postcss-uncss");
+
+gulp.task("style", function () {
+  gulp.src("sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer({
+        browsers: [
+        "last 50 versions"
+      ]
+      })
+    ]))
+    .pipe(gulp.dest("css"))
+});
+
+gulp.task("img", function () {
+  return gulp.src("img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+    imagemin.gifsicle({
+        interlaced: true
+      }),
+    imagemin.jpegtran({
+        progressive: true
+      }),
+    imagemin.optipng({
+        optimizationLevel: 7
+      })
+]))
+    .pipe(gulp.dest("build/img"))
+});
+
+gulp.task("js", function () {
+  return gulp.src(["js/jquery.min.js", "js/bootstrap.min.js", "js/script.js"])
+    .pipe(concat("script.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("js"));
+});
+
+gulp.task("css", function () {
+  var plugins = [
+    uncss({
+      html: ["index.html"],
+      ignore: [".fade",
+                  ".fade.in",
+                  ".collapse",
+                  ".collapse.in",
+                  ".collapsing",
+                  ".alert-danger",
+                  /\.open/
+                ]
+    }),
+    ];
+  return gulp.src(["css/bootstrap.css", "css/style.css"])
+    .pipe(concat("style.min.css"))
+    .pipe(postcss(plugins))
+    .pipe(combineMq())
+    .pipe(cssmin())
+    .pipe(gulp.dest("css"));
+});
+
+gulp.task("min", ["js", "css"]);
+
+gulp.task("ht", function () {
+  return gulp.src("index.html")
+    .pipe(htmlmin({
+      removeComments: true,
+      collapseWhitespace: true,
+      preserveLineBreaks: true
+    }))
+    .pipe(gulp.dest(""));
+});
+
+gulp.task("default", ["style"], function () {
+  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+});
